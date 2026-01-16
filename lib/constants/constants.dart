36543b4f3fd334bbca8e8f125c67b6aa26883e78
aakashsahh <aakashsah181@gmail.com>
2026-01-16 10:06:52 +0545

    feat: add application constants Define API endpoints, storage keys, and timing constants

---
+++ lib/constants/constants.dart
@@ -0,0 +1,12 @@
+class AppConstants {
+  // API
+  static const String apiBaseUrl = 'http://api.weatherapi.com/v1';
+  static const String apiKey = 'ee179721f4374ba8876150949261501';
+
+  // Storage Keys
+  static const String helpSkippedKey = 'help_skipped';
+  static const String savedLocationKey = 'saved_location';
+
+  // Timing
+  static const int helpScreenDuration = 5; // seconds
+}
